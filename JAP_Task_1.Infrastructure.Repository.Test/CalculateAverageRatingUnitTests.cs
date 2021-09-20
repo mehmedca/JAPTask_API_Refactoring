503967559e92cfa68c2c6c28d3362a8b08f9ad9c
Mehmed Catic <mehmedca@maestralsolutions.com>
2021-09-20 21:19:36 +0200

    Added Test project, implemented unit tests for AverageMovieRatings and MovieScreenings

---
+++ JAP_Task_1.Infrastructure.Repository.Test/CalculateAverageRatingUnitTests.cs
@@ -0,0 +1,118 @@
+﻿using AutoMapper;
+using JAP.Core.Entities;
+using JAP.Core.Interfaces.IRepository;
+using JAP.Database.Context;
+using JAP.Mapper;
+using JAP.Repository;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAP_Task_1.Infrastructure.JAP.Repository.Test
+{
+    public class CalculateAverageRatingUnitTests
+    {
+        private JAPContext _context { get; set; }
+        private IMapper _mapper { get; set; }
+        private Mock<IScreeningsRepository> mockScreeningRepo { get; set; }
+        private Mock<IRatingRepository> mockRatingRepo { get; set; }
+
+        private Movie movie1 { get; set; }
+        private Movie movie2 { get; set; }
+
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder().UseInMemoryDatabase("testdb");
+            _context = new JAPContext(dbContextOptions.Options);
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(typeof(ModelsToEntitiesProfiles)));
+            _mapper = new Mapper(configuration);
+
+            mockScreeningRepo = new Mock<IScreeningsRepository>();
+            mockRatingRepo = new Mock<IRatingRepository>();
+
+
+            movie1 = new Movie
+            {
+                Id = 1,
+                Title = "Test title",
+                Description = "Test description",
+                ReleaseDate = new DateTime(2020, 10, 05),
+                RatingTotal = 0
+            };
+            await _context.Movies.AddAsync(movie1);
+
+            for (int i = 0; i < 4; i++)
+            {
+                await _context.Ratings.AddAsync(new Rating
+                {
+                    Id = i + 1,
+                    MovieId = movie1.Id,
+                    RatedById = "0000-1111",
+                    RatingInt = i + 2
+                });
+            }
+            await _context.SaveChangesAsync();
+
+
+            movie2 = new Movie
+            {
+                Id = 2,
+                Title = "Test title",
+                Description = "Test description",
+                ReleaseDate = new DateTime(2020, 10, 05),
+                RatingTotal = 0
+            };
+            await _context.Movies.AddAsync(movie2);
+
+            for (int i = 5; i < 8; i++)
+            {
+                await _context.Ratings.AddAsync(new Rating
+                {
+                    Id = i,
+                    MovieId = movie2.Id,
+                    RatedById = "0000-1111",
+                    RatingInt = 4
+                });
+            }
+            await _context.SaveChangesAsync();
+        }
+
+
+
+        [Test]
+        public void SetMovieRatingTotalAsync_SetRatingToNonExistingMovie_ThrowsException()
+        {
+            var movieRepo = new MovieRepository(_context, _mapper, mockRatingRepo.Object, mockScreeningRepo.Object);
+
+            Assert.ThrowsAsync<Exception>(async () => await movieRepo.SetMovieRatingTotalAsync(99));
+        }
+
+        [Test]
+        public async Task SetMovieRatingTotalAsync_SetFourRatingsToMovie_ResultEqualTo_3_5()
+        {
+            var movieRepo = new MovieRepository(_context, _mapper, mockRatingRepo.Object, mockScreeningRepo.Object);
+
+            await movieRepo.SetMovieRatingTotalAsync(movie1.Id);
+
+            Assert.That(movie1.RatingTotal, Is.EqualTo(3.5));
+        }
+
+        [Test]
+        public async Task SetMovieRatingTotalAsync_SetThreeRatingsToMovie_ResultEqualTo_4()
+        {
+            var movieRepo = new MovieRepository(_context, _mapper, mockRatingRepo.Object, mockScreeningRepo.Object);
+
+            await movieRepo.SetMovieRatingTotalAsync(movie2.Id);
+
+            Assert.That(movie2.RatingTotal, Is.EqualTo(4));
+        }
+    }
+}
---
+++ JAP_Task_1.Infrastructure.Repository.Test/ScreeningRepositoryUnitTests.cs
@@ -0,0 +1,144 @@
+using AutoMapper;
+using JAP.Core.Entities;
+using JAP.Core.Interfaces.IRepository;
+using JAP.Database.Context;
+using JAP.Mapper;
+using JAP.Repository;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JAP_Task_1.Infrastructure.JAP.Repository.Test
+{
+    public class ScreeningRepositoryUnitTests
+    {
+        private JAPContext _context { get; set; }
+        private IMapper _mapper{ get; set; }
+        private Screening  screeningInThePast { get; set; }
+        private Screening  screeningInTheFutureWithAvailableTickets { get; set; }
+        private Screening  screeningInTheFutureWithoutTickets { get; set; }
+        private Screening  screeningInTheFutureWithTicketsThatAreSold { get; set; }
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder().UseInMemoryDatabase("testdb");
+            _context = new JAPContext(dbContextOptions.Options);
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(typeof(ModelsToEntitiesProfiles)));
+            _mapper = new Mapper(configuration);
+
+
+            screeningInThePast = new Screening
+            {
+                Id = 1,
+                MovieId = 1,
+                StartDate = new DateTime(2021, 09, 15, 21, 30, 0),
+                EndDate = new DateTime(2021, 09, 15, 23, 30, 0)
+            };
+            await _context.Screenings.AddAsync(screeningInThePast);
+            await _context.SaveChangesAsync();
+
+            for (int i = 0; i < 5; i++)
+            {
+                await _context.Tickets.AddAsync(new Ticket
+                {
+                    IsSold = false,
+                    Price = i + 5,
+                    ScreeningId = 1
+                });
+            }
+            await _context.SaveChangesAsync();
+
+
+            screeningInTheFutureWithAvailableTickets = new Screening
+            {
+                Id = 2,
+                MovieId = 1,
+                StartDate = new DateTime(2021, 11, 10, 21, 30, 0),
+                EndDate = new DateTime(2021, 11, 10, 23, 30, 0)
+            };
+            await _context.Screenings.AddAsync(screeningInTheFutureWithAvailableTickets);
+            await _context.SaveChangesAsync();
+
+            for (int i = 0; i < 5; i++)
+            {
+                await _context.Tickets.AddAsync(new Ticket
+                {
+                    IsSold = false,
+                    Price = i + 5,
+                    ScreeningId = 2
+                });
+            }
+            await _context.SaveChangesAsync();
+
+
+            screeningInTheFutureWithoutTickets = new Screening
+            {
+                Id = 3,
+                MovieId = 1,
+                StartDate = new DateTime(2021, 09, 15, 21, 30, 0),
+                EndDate = new DateTime(2021, 09, 15, 23, 30, 0)
+            };
+            await _context.Screenings.AddAsync(screeningInTheFutureWithoutTickets);
+            await _context.SaveChangesAsync();
+
+
+            screeningInTheFutureWithTicketsThatAreSold = new Screening
+            {
+                Id = 4,
+                MovieId = 1,
+                StartDate = new DateTime(2021, 11, 10, 21, 30, 0),
+                EndDate = new DateTime(2021, 11, 10, 23, 30, 0)
+            };
+            await _context.Screenings.AddAsync(screeningInTheFutureWithTicketsThatAreSold);
+            await _context.SaveChangesAsync();
+
+            for (int i = 0; i < 5; i++)
+            {
+                await _context.Tickets.AddAsync(new Ticket
+                {
+                    IsSold = true,
+                    Price = i + 5,
+                    ScreeningId = 4
+                });
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        [Test]
+        public void BuyTicketsAsync_TryToBuyTicketForScreeningInThePast_ThrowsException()
+        {
+            var scrRepo = new ScreeningsRepository(_context, _mapper);
+
+            Assert.ThrowsAsync<Exception>(async () => await scrRepo.BuyTicketAsync(screeningInThePast.Id));
+        }
+
+        [Test]
+        public void BuyTicketsAsync_TryToBuyTicketForScreeningInTheFutureWithAvailableTickets_ResultPasses()
+        {
+            var scrRepo = new ScreeningsRepository(_context, _mapper);
+
+            Assert.DoesNotThrowAsync(async () => await scrRepo.BuyTicketAsync(screeningInTheFutureWithAvailableTickets.Id));
+        }
+
+        [Test]
+        public void BuyTicketsAsync_TryToBuyTicketForScreeningInTheFutureWithoutExistingTickets_ThrowsException()
+        {
+            var scrRepo = new ScreeningsRepository(_context, _mapper);
+
+            Assert.ThrowsAsync<Exception>(async () => await scrRepo.BuyTicketAsync(screeningInTheFutureWithoutTickets.Id));
+        }
+
+        [Test]
+        public void BuyTicketsAsync_TryToBuyTicketForScreeningInTheFutureWithSoldOutTickets_ThrowsException()
+        {
+            var scrRepo = new ScreeningsRepository(_context, _mapper);
+
+            Assert.ThrowsAsync<Exception>(async () => await scrRepo.BuyTicketAsync(screeningInTheFutureWithTicketsThatAreSold.Id));
+        }
+    }
+}
